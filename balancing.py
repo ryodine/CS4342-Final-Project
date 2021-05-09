55076b0a002308b260f326604e973543e06b2573
alescion <33096482+alescion@users.noreply.github.com>
2021-05-09 11:34:09 -0400

    implemented function to generate a minimum balanced dataset

--- balancing.py
+++ balancing.py
@@ -14,6 +14,7 @@ def countlabels():
 
         return counts
 
+# helper function
 # given a new set of image_ids, create a corresponding set of images
 # images are selectively copied from the original set into a new directory
 def getimages(ids, dirname):
@@ -24,15 +25,36 @@ def getimages(ids, dirname):
         os.mkdir(dest)
 
         for id in ids:
-            img = "%s.jpg" % (id)
-            shutil.copy(os.path.join(imgs, img), dest)
+            # img = "%s.jpg" % (id)
+            # shutil.copy(os.path.join(imgs, img), dest)
+            shutil.copy(os.path.join(imgs, id), dest)
         
     except Exception as e:
         print(e)
         print("Could not create image set (if the directory already exists, delete/rename it and try again)")
 
-def minimize(): # TODO: make a minimum balanced set of training images
-    pass
+# create a minimum balanced set of training images
+# TODO: shuffle the rows in train.csv so a different minimized dataset is produced every time?
+def minimize():
+    dist = countlabels()
+    minimum = dist[min(dist)] # gets the value of the class with least amt of labels in the set
 
-ids = [6103, 2272550, 690163]
-getimages(ids, "TESTLOL")
+    with open("train.csv") as labels:
+        reader = csv.reader(labels)
+        header = next(reader)
+        ids = []
+        counts = {key: 0 for key in ['0','1','2','3','4']}
+        for row in reader:
+            # id = row[0][:-4] # remove '.jpg'
+            id = row[0]
+            label = row[1]
+
+            if counts[label] < minimum:
+                ids.append(id)
+                counts[label] += 1
+
+        getimages(ids, "min_balanced_train_imgs")
+        return counts
+
+
+# print(minimize())
