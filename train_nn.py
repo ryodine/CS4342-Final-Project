e5ce66feef9dc1b2ec2d00c3679635a246c67ae4
Ryan Johnson <ryan@johnsonweb.us>
2021-05-09 20:48:51 -0400

    NN train at 74%

--- train_nn.py
+++ train_nn.py
@@ -1,13 +1,10 @@
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.client import device_lib
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Activation, Dropout, LayerNormalization
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Activation, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
-
-##################
-# Threading
-tf.config.threading.set_inter_op_parallelism_threads(10)
 
 ############################################################
 #
@@ -19,7 +16,7 @@ df = pd.read_csv("train.csv").astype(str)
 
 splits = np.split(df, [6*len(df)//10, 8*len(df)//10])
 
-batch_size = 32
+batch_size = 16
 
 # Training set will have some data augmentation
 train_datagen = ImageDataGenerator(
@@ -41,7 +38,7 @@ train_generator = train_datagen.flow_from_dataframe(
     batch_size=batch_size,
     class_mode='categorical')
 
-validation_generator = train_datagen.flow_from_dataframe(
+validation_generator = test_datagen.flow_from_dataframe(
     splits[1],
     'train_images',
     x_col='image_id',
@@ -50,6 +47,15 @@ validation_generator = train_datagen.flow_from_dataframe(
     batch_size=batch_size,
     class_mode='categorical')
 
+
+test_generator = test_datagen.flow_from_dataframe(
+    splits[2],
+    'train_images',
+    x_col='image_id',
+    y_col='label',
+    target_size=(150, 150),
+    batch_size=batch_size,
+    class_mode='categorical')
 
 ############################################################
 #
@@ -60,32 +66,30 @@ validation_generator = train_datagen.flow_from_dataframe(
 #create model
 model = Sequential()
 #add model layer
-
-"""
-model.add(Conv2D(50, kernel_size=15, input_shape=(150, 150, 3)))
-model.add(Activation('relu'))
-model.add(LayerNormalization())
-model.add(MaxPooling2D(pool_size=(5, 5)))
-
-model.add(Conv2D(255, (3, 3)))
-model.add(Activation('relu'))
-model.add(LayerNormalization())
-model.add(MaxPooling2D(pool_size=(5, 5)))
-
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(5))
-model.add(Activation('softmax'))
-"""
-
-"""
 model.add(Conv2D(32, kernel_size=7, input_shape=(150, 150, 3)))
+"""
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(5, activation='softmax'))
+"""
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -99,28 +103,6 @@ model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(5))
 model.add(Activation('softmax'))
-"""
-
-
-model.add(Conv2D(32, kernel_size=15, input_shape=(150, 150, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(5, 5)))
-
-model.add(Conv2D(32, kernel_size=7))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(3, 3)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(3, 3)))
-
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(5))
-model.add(Activation('softmax'))
-
 
 # the model so far outputs 3D feature maps (height, width, features)
 
@@ -130,7 +112,7 @@ model.compile(optimizer='adam', loss='categorical_crossentropy',
 
 ############################################################
 #
-#   Model
+#   Model Training
 #
 ############################################################
 
@@ -142,7 +124,6 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     mode='max',
     save_best_only=True)
 
-"""
 model_early_stop_callback = tf.keras.callbacks.EarlyStopping(
     monitor="val_loss",
     min_delta=0,
@@ -152,9 +133,8 @@ model_early_stop_callback = tf.keras.callbacks.EarlyStopping(
     baseline=None,
     restore_best_weights=False,
 )
-"""
 
 model.fit(x=train_generator,
           validation_data=validation_generator,
           callbacks=[model_checkpoint_callback],
-          epochs=150)
+          epochs=300)
